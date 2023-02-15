b644f8c7716a2544a2932e714f2dc797f4fb708b
ElPediatra <albertogomezmorales1994@gmail.com>
2023-02-15 17:38:04 +0100

    Contador Android Studio
    Alberto Gómez Morales
    1º DAW
    I.E.S. Murgi

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void botonClick(View view){
+        TextView contador = findViewById(R.id.txtContar);
+        contador.setText(String.valueOf(i));
+        i++;
     }
 }
